18016fb28ff3d30f5f217cf5410187b366785a2d
sara sallat <sallatsara@gmail.com>
2022-10-21 01:04:24 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+ function superbowlWin(record){
+    const result=record.find(item => item.result==="W")
+        if(result){
+            return result.year
+        }
+        else{
+            return undefined
+        }
+        
+    }
